6e63b980cfe48143b949f0569d04af679af48588
Cristian Castilla <155536171+CristianCastilla@users.noreply.github.com>
2025-03-26 18:06:38 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado. Nos pondremos en contacto con usted pronto.');
+    // Aquí puedes agregar el código para enviar el formulario a tu servidor
+});
